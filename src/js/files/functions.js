7189f3fc99a7a23ac1e50588f8d943e54ba5d3fa
BMandruxa <riki-chiki5@yandex.ru>
2022-04-08 16:32:07 +0300

    menu

--- src/js/files/functions.js
+++ src/js/files/functions.js
@@ -30,11 +30,25 @@ const isMobile = {
 if (isMobile.any()) {
 	document.body.classList.add('_touch');
 	let titleOne = document.querySelector('#title-1');
+	const serviceOne = document.querySelector('.serviceOne');
+
 	if (titleOne) {
 		titleOne.addEventListener("click", function (e) {
 			titleOne.classList.toggle('_active');
 		});
 	}
+	if (serviceOne) {
+		serviceOne.addEventListener("click", function (e) {
+			serviceOne.classList.toggle('_open');
+		});
+	}
+
 } else {
 	document.body.classList.add('_pc');
+	const serviceOne = document.querySelector('.serviceOne');
+	if (serviceOne) {
+		serviceOne.addEventListener("click", function (e) {
+			serviceOne.classList.toggle('_open');
+		});
+	}
 }
