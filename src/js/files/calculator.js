1db06cf0ec682397e1a60484ad3016f0537d6727
BMandruxa <riki-chiki5@yandex.ru>
2022-04-19 22:11:50 +0300

    correction number two

--- src/js/files/calculator.js
+++ src/js/files/calculator.js
@@ -2,11 +2,13 @@
 const radio = document.querySelectorAll(".checkb");
 const width = document.querySelectorAll(".calculator__item");
 const window = document.querySelectorAll(".calculator__window");
+const number = document.querySelectorAll(".calculator__left");
 if (radio) {
    radio.forEach((item) =>
       item.addEventListener("click", () => {
          item.classList.toggle("_click");
          width.forEach((wid) => wid.classList.toggle("_active"));
+         number.forEach((num) => num.classList.toggle("_active"));
       })
    );
 }
@@ -15,6 +17,7 @@ if (window) {
       itemTwo.addEventListener("click", () => {
          itemTwo.classList.toggle("_click");
          width.forEach((wid) => wid.classList.toggle("_active"));
+         number.forEach((num) => num.classList.toggle("_active"));
       })
    );
 }
